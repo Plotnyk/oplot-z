db2be0dc42686349317cbcd6bb04604103f23af9
Plotnyk <YeP2809889@>
2018-08-26 00:52:08 +0300

    Обновление модели и тестов

--- model/src/main/java/app/model/entity/geography/Country.java
+++ model/src/main/java/app/model/entity/geography/Country.java
@@ -35,6 +35,24 @@ public class Country extends AbstractEntity {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Country country = (Country) o;
+        return Objects.equals(name, country.name) &&
+                Objects.equals(codePhone, country.codePhone) &&
+                Objects.equals(coordinate, country.coordinate) &&
+                Objects.equals(regions, country.regions);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(super.hashCode(), name, codePhone);
+    }
+
+    @Override
     public String toString() {
         return "Country{" +
                 "name='" + name + '}';
--- model/src/test/java/app/model/entity/geography/CountryTest.java
+++ model/src/test/java/app/model/entity/geography/CountryTest.java
@@ -25,28 +25,48 @@ public class CountryTest {
 
     @Test
     public void testAddDublicateRegionSuccess() {
+        country.addRegion("Київська");
+        country.addRegion("Київська");
+
+        assertEquals(country.getRegions().size(), 1);
     }
 
     @Test(expected = NullPointerException.class)
     public void testAddNullRegionSuccess() {
+        country.addRegion(null);
+        fail();
     }
 
     @Test
     public void testRemoveRegionSuccess() {
+        Region region = country.addRegion("Київська");
+        country.removeRegion(region);
+        assertTrue(country.getRegions().isEmpty());
     }
 
     @Test(expected=NullPointerException.class)
     public void testRemoveNullRegionSuccess() {
+        country.removeRegion(null);
+        fail();
     }
 
     @Test
     public void testEqualRegionSuccess() {
+        Region region = country.addRegion("Київська");
+        Region region1 = country.addRegion("Київська");
 
+        assertEquals(region, region1);
     }
 
     @Test
     public void testEqualRegionFail() {
+        Region region = country.addRegion("Київська");
+        Region region1 = country.addRegion("Харковская");
+        Country country1 = new Country("Белорусь");
+        Region region2 = country1.addRegion("Київська");
 
+        assertNotEquals(region, region1);
+        assertNotEquals(region, region2);
     }
 
     private boolean containsRegion(Country country, Region region) {
